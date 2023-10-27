769823e467f67ab0bea6acf989063a5eb540c407
Rohan Tiwari <genius433@gmail.com>
2023-10-27 10:03:48 -0700

    Hiding the cpp implemenation from package's external interface. Using const in cpp code."

--- src/univariatemhnormal.cpp
+++ src/univariatemhnormal.cpp
@@ -8,23 +8,27 @@ using namespace Rcpp;
 //' @param initial This is the starting value for the markov chain
 //' @param sigma This is the standard deviation for candidate generating univariate normal distribution.
 // [[Rcpp::export]]
-NumericVector univariatemhnormalcpp(Function targetDensity, NumericVector numIter,
-                                    NumericVector initial, NumericVector sigma) {
+NumericVector univariatemhnormalcpp(Function targetDensity, const NumericVector numIter,
+                                    const NumericVector initial,
+                                    const NumericVector sigma) {
   // skip checks because they are done in R before call
   // initialize the x vector to capture the generated samples
-  NumericVector x(numIter[0]);
-  x[0] = initial[0];
+  const float init = initial[0];
+  const float sd = sigma[0];
+  const int n = numIter[0];
+  NumericVector x(n);
+  x[0] = init;
   //build the markov chain
-  for(int i = 2; i <= numIter[0]; i++){
+  for(int i = 2; i <= n; i++){
     //generate a  possible move in the markov chain
-    NumericVector nextVal = rnorm(1, x[i-1], sigma[0]);
+    const NumericVector nextVal = rnorm(1, x[i-1], sd);
     //compute the acceptance probability
     //this is a special case because the candidate is symmetric
-    NumericVector prevDensity = targetDensity(x[i-1]);
-    NumericVector nextDensity = targetDensity(nextVal[0]);
+    const NumericVector prevDensity = targetDensity(x[i-1]);
+    const NumericVector nextDensity = targetDensity(nextVal[0]);
 
-    double prob = std::min(1.0, nextDensity[0] / prevDensity[0]);
-    NumericVector u = runif(1);
+    const double prob = std::min(1.0, nextDensity[0] / prevDensity[0]);
+    const NumericVector u = runif(1);
     //accept the sample with 'prob' probability
     if(u[0] <= prob){
       x[i] = nextVal[0];
