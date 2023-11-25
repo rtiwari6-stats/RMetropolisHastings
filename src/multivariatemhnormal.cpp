92ed0f4ea60b5898fa3cb413b97c5df4796010cf
Skylar Liu <slliu@tamu.edu>
2023-11-25 17:02:44 -0600

    converted multivariatemhexpnormalcpp using multivariate case

--- src/multivariatemhnormal.cpp
+++ src/multivariatemhnormal.cpp
@@ -3,10 +3,10 @@
 
 using namespace Rcpp;
 
-//' C++ function to generate a probability from a univariate exponential density function
+//' C++ function to generate a probability from a multivariate exponential density function
 //' @param x This is the x value
 // [[Rcpp::export]]
-double targetDensityUnivarExp(float x){
+double targetDensityMultivarExp(float x){
   if (x < 0.0){
     return 0.0f;
   }
@@ -15,34 +15,34 @@ double targetDensityUnivarExp(float x){
   }
 }
 
-//' C++ function to generate samples using univariate normal candidate density
+//' C++ function to generate samples using multivariate normal candidate density
 //' @param numIter This is the number of iterations to run the algorithm
-//' @param initial This is the starting value for the markov chain
-//' @param sigma This is the standard deviation for candidate generating univariate normal distribution.
+//' @param initial_vec This is the starting vector for the markov chain
+//' @param sigma_matrix This is the standard deviation matrix for candidate generating multivariate normal distribution.
 // [[Rcpp::export]]
-NumericVector univariatemhexpnormalcpp(NumericVector numIter,
-                                       NumericVector initial, NumericVector sigma) {
+NumericMatrix multivariatemhexpnormalcpp(NumericVector numIter,
+                                       NumericVector initial_vec, NumericMatrix sigma_matrix) {
   // skip rest checks because they are done in R before call
-  // initialize the x vector to capture the generated samples
-  NumericVector x(numIter[0]);
-  x[0] = initial[0];
+  // initialize the x matrix to capture the generated samples
+  NumericMatrix x(numIter, size(initial_vec));
+  x[0, ] = initial_vec;
   //build the markov chain
   for(int i = 2; i <= numIter[0]; i++){
     //generate a  possible move in the markov chain
-    NumericVector nextVal = rnorm(1, x[i-1], sigma[0]);
+    NumericVector nextVal = mvnorm(1, x[i-1, ], sigma_matrix[0, ]);
     //compute the acceptance probability
     //this is a special case because the candidate is symmetric
-    double prevDensity = targetDensityUnivarExp(x[i-1]);
+    double prevDensity = targetDensityUnivarExp(x[i-1, ]);
     double nextDensity = targetDensityUnivarExp(nextVal[0]);
 
     double prob = std::min(1.0, nextDensity / prevDensity);
     NumericVector u = runif(1);
     //accept the sample with 'prob' probability
     if(u[0] <= prob){
-      x[i] = nextVal[0];
+      x[i, ] = nextVal[0];
     }
     else{
-      x[i] = x[i-1];
+      x[i, ] = x[i-1, ];
     }
   }
   return x;
